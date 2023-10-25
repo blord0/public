09607c2ca1301231ee86eb91773951fc39bae7a9
blord0 <68508813+blord0@users.noreply.github.com>
2023-10-25 01:58:16 +0100

    Create dm_user.py

---
+++ dm_user.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+dm_user=int(input("Input recipient user id:\n"))
+dm_content=input("Input what message you want to send to them:\n")
+
+bot = discord.Client(intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+  user=bot.get_user(dm_user)
+  dm = await user.create_dm()
+  await dm.send(dm_content)
+  print(f"Sent message to {user}")
+  print(f"Sent '{dm_content}'")
+  os.kill(os.getpid(), 9)
+
+bot.run(token)
